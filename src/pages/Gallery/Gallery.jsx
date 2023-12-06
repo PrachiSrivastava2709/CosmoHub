53278b66742eb7073e760fb2a0d2a9137c57ab18
Prachi <sriprachi2709@gmail.com>
2023-12-06 19:18:42 +0530

    gallery work started

--- src/pages/Gallery/Gallery.jsx
+++ src/pages/Gallery/Gallery.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import React from 'react';
+import Data from './data.js';
+import Card from '../../components/Card.jsx';
 
 export default function Gallery() {
-  return (
-	<div>Gallery</div>
-  )
+	function createCard(item){
+		return (
+			<Card 
+				image= {item.links[0].href}
+				date= {item.data[0].date_created}
+				title= {item.data[0].title}
+			/>
+		)
+	}
+
+
+	return (
+		<>
+			<div className="grid-container">
+				{Data.collection.items.map(createCard)}
+			</div>
+		</>
+
+	)
 }
